ff8d6e11d01de6b4de5b40f9427a4837f94f56e0
Raven <bloodraven166@gmail.com>
2021-04-11 02:54:46 +0530

    Update Conjugation.jl

--- src/Conjugation.jl
+++ src/Conjugation.jl
@@ -1,17 +1,19 @@
 # Author: Anirban166 | Last updated: 11/04/21
-println("Enter a French verb") 
-verb = readline()
-base = chop(verb, tail = 2)
-if endswith(verb, "er")
-  println("Je ", base, "e\n",
-          "Tu ", base, "es\n",
-          "Il ", base, "e\n",  
-          "Elle ", base, "e\n",
-          "Ils ", base, "ent\n",  
-          "Elles ", base, "ent\n",
-          "Vous ", base, "ez\n",
-          "Nous ", base, "ons")  
-elseif endswith(verb, "ir")
+println("Enter a French verb / Entrez un verbe français") 
+verbFR = readline()
+base = chop(verbFR, tail = 2)
+println("Enter its English translation / Entrez sa traduction en anglais")
+verbEN = readline()
+if endswith(verbFR, "er")
+  println("Je ", base, "e (I ", verbEN, ")\n",
+          "Tu ", base, "es (You ", verbEN, " - informal)\n",
+          "Il ", base, "e (He ", verbEN, "s)\n",  
+          "Elle ", base, "e (She ", verbEN, "s)\n",
+          "Ils ", base, "ent (They ", verbEN, " - male)\n",  
+          "Elles ", base, "ent (They ", verbEN, " - female)\n",
+          "Vous ", base, "ez (You ", verbEN, " - formal)\n",
+          "Nous ", base, "ons (We ", verbEN, ")")  
+elseif endswith(verbFR, "ir")
   println("Je ", base, "is\n",
           "Tu ", base, "is\n",
           "Il ", base, "it\n",
@@ -20,7 +22,7 @@ elseif endswith(verb, "ir")
           "Elles ", base, "issent\n",
           "Vous ", base, "issez\n",
           "Nous ", base, "issons") 
-elseif endswith(verb, "re")
+elseif endswith(verbFR, "re")
   println("Je ", base, "s\n",
           "Tu ", base, "s\n",
           "Il ", base, "\n", 
