c482e670d8a38626307b5e1fa524a440925b466c
Raven <bloodraven166@gmail.com>
2021-04-11 16:17:37 +0530

    Update Conjugation.jl

--- src/Conjugation.jl
+++ src/Conjugation.jl
@@ -1,36 +1,49 @@
 # Author: Anirban166 | Last updated: 11/04/21
-println("Enter a French verb / Entrez un verbe français") 
-verbFR = readline()
-base = chop(verbFR, tail = 2)
-println("Enter its English translation / Entrez sa traduction en anglais")
-verbEN = readline()
-if endswith(verbFR, "er")
-  println("Je ", base, "e (I ", verbEN, ")\n",
-          "Tu ", base, "es (You ", verbEN, " - informal)\n",
-          "Il ", base, "e (He ", verbEN, "s)\n",  
-          "Elle ", base, "e (She ", verbEN, "s)\n",
-          "Ils ", base, "ent (They ", verbEN, " - male)\n",  
-          "Elles ", base, "ent (They ", verbEN, " - female)\n",
-          "Vous ", base, "ez (You ", verbEN, " - formal)\n",
-          "Nous ", base, "ons (We ", verbEN, ")")  
-elseif endswith(verbFR, "ir")
-  println("Je ", base, "is\n",
-          "Tu ", base, "is\n",
-          "Il ", base, "it\n",
-          "Elle ", base, "it\n",
-          "Ils ", base, "issent\n", 
-          "Elles ", base, "issent\n",
-          "Vous ", base, "issez\n",
-          "Nous ", base, "issons") 
-elseif endswith(verbFR, "re")
-  println("Je ", base, "s\n",
-          "Tu ", base, "s\n",
-          "Il ", base, "\n", 
-          "Elle ", base, "\n", 
-          "Ils ", base, "ent\n", 
-          "Elles ", base, "ent\n",
-          "Vous ", base, "ez\n",
-          "Nous ", base, "ons") 
-else  
-  println("Unknown verb suffix!")
+while true
+  println("Enter a French verb / Entrez un verbe français") 
+  verbFR = readline()
+  base = chop(verbFR, tail = 2)
+  println("Enter its English translation / Entrez sa traduction en anglais")
+  verbEN = readline()
+  if endswith(verbFR, "er")
+    println("Je ", base, "e (I ", verbEN, ")\n",
+            "Tu ", base, "es (You ", verbEN, " - informal)\n",
+            "Il ", base, "e (He ", verbEN, "s)\n",  
+            "Elle ", base, "e (She ", verbEN, "s)\n",
+            "Ils ", base, "ent (They ", verbEN, " - male)\n",  
+            "Elles ", base, "ent (They ", verbEN, " - female)\n",
+            "Vous ", base, "ez (You ", verbEN, " - formal)\n",
+            "Nous ", base, "ons (We ", verbEN, ")")  
+  elseif endswith(verbFR, "ir")
+    println("Je ", base, "is (I ", verbEN, ")\n",
+            "Tu ", base, "is (You ", verbEN, " - informal)\n",
+            "Il ", base, "it (He ", verbEN, "s)\n",
+            "Elle ", base, "it (She ", verbEN, "s)\n",
+            "Ils ", base, "issent (They ", verbEN, " - male)\n", 
+            "Elles ", base, "issent (They ", verbEN, " - female)\n",
+            "Vous ", base, "issez (You ", verbEN, " - formal)\n",
+            "Nous ", base, "issons (We ", verbEN, ")") 
+  elseif endswith(verbFR, "re")
+    println("Je ", base, " (I ", verbEN, ")\n",
+            "Tu ", base, "s (You ", verbEN, " - informal)\n",
+            "Il ", base, " (He ", verbEN, "s)\n", 
+            "Elle ", base, " (She ", verbEN, "s)\n", 
+            "Ils ", base, "ent (They ", verbEN, " - male)\n", 
+            "Elles ", base, "ent (They ", verbEN, " - female)\n",
+            "Vous ", base, "ez (You ", verbEN, " - formal)\n",
+            "Nous ", base, "ons (We ", verbEN, ")") 
+  else  
+    println("Unknown verb suffix!")
+  end
+  @label checkpoint
+  println("Would you like to try another verb? (Yes/No) / Souhaitez-vous essayer un autre verbe? (Oui/Non)") 
+  choice = readline()
+  if issubset([choice], ["Yes", "yes", "Oui", "oui"])
+    continue
+  elseif issubset([choice], ["No", "no", "Non", "non"])
+    break
+  else
+    @warn "Please enter Yes/No! / Veuillez entrer Oui/Non!"
+    @goto checkpoint
+  end
 end
